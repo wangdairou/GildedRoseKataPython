2095116ea252aeba05866b9779fb92806cf2b926
Dairou Wang <dairou2021@gmail.com>
2024-10-01 20:03:01 -0700

    Lab 4

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -11,6 +11,23 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEquals("fixme", items[0].name)
 
+    def test_quality_change(self):
+        items = [Item("Some item", 5, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(19, items[0].quality)
+
+    def test_aged_brie(self):
+        items = [Item("Aged Brie", 100, 5)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(6, items[0].quality)
+
+    def test_backstage_passes(self):
+        items = [Item("Backstage passes", 100, 5)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(6, items[0].quality)
 
 if __name__ == '__main__':
     unittest.main()
